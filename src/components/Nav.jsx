b970f4dbba23b3632419966362fce96c1b23ac04
Jaden Phan <jadenphan1010@gmail.com>
2026-06-13 14:37:38 -0700

    added honed-disc icon

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,13 @@
 import styles from './Nav.module.css'
+import honedMark from '../assets/honed-mark.svg'
 
 export default function Nav() {
   return (
     <nav className={styles.nav}>
-      <a href="#top" className={styles.logo}>Honed</a>
+      <a href="#top" className={styles.logo}>
+        <img src={honedMark} alt="" aria-hidden="true" className={styles.mark} />
+        Honed
+      </a>
       <div className={styles.links}>
         <a href="#pledge" className={styles.link}>The pledge</a>
         <a href="#how" className={styles.link}>How it works</a>
