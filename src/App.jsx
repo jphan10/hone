c2200caee6df918ddf17769468a99324b1db5265
Jaden Phan <jadenphan1010@gmail.com>
2026-06-13 14:38:55 -0700

    importing analytics package

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import HowItWorks from './components/HowItWorks'
 import Marketplace from './components/Marketplace'
 import Waitlist from './components/Waitlist'
 import Footer from './components/Footer'
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App() {
   return (
